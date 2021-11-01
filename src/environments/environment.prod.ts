98623bfe4eb438e5bf7ca1de6f6bb79838a2e762
unknown <sauvole75@gmail.com>
2021-11-01 14:18:40 +0700

    add

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebaseConfig : {
+    apiKey: "AIzaSyDObAxKVnldv9c1ArB8htzZ07WZuk-tPso",
+    authDomain: "nguyenthan2009-6632e.firebaseapp.com",
+    projectId: "nguyenthan2009-6632e",
+    storageBucket: "nguyenthan2009-6632e.appspot.com",
+    messagingSenderId: "118204205848",
+    appId: "1:118204205848:web:c032e8e0b6701a90082c6d",
+    measurementId: "G-SVL6TCW94C"
+  },
+  production: true
+};
